9d7a46fd692f5bdae55eeec7ef8badb3ea02850c
Chetanya Sharma <chetanya.s11@gmail.com>
2015-08-25 10:14:48 +0100

    CS - Value State Demo

--- application/detail/valueStateDemoPage.controller.js
+++ application/detail/valueStateDemoPage.controller.js
@@ -15,6 +15,54 @@ sap.ui.controller("application.detail.valueStateDemoPage", {
 	},
 
 	onBeforeShow : function(evt){
+		//form model
+		var contact = {
+			name: "",
+			designation: [{text: "Developer"}, {text: "Manager"}, {text: "Consultant"}],
+			houseNo: "",
+			street: ""
+		}
 
+		var oModel = new sap.ui.model.json.JSONModel(contact);
+		sap.ui.getCore().setModel(oModel, "contact");
+
+	},
+
+	handleValidateForm: function(evt){
+		var inputs = [
+	    valueStateController.getView().byId("Input1"),
+	    valueStateController.getView().byId("Input2"),
+	    valueStateController.getView().byId("Input3")
+	  ];
+	  // noprotect
+	  for(var i=0; i<inputs.length; i++){
+	    if (inputs[i].getValue() === "" ||
+	        inputs[i].getValue() === undefined ||
+	        inputs[i].getValue() === 0) {
+	      inputs[i].setValueState("Error");
+	      inputs[i].setValueStateText("Please enter a value");
+	    }else {
+	      inputs[i].setValueState("Success");
+	    }
+	  }
+	  // check states of inputs
+	  // var canContinue = true;
+	  // for(var j=0;j<inputs.length;j++){
+	  //   if ("Error" === inputs[j].getValueState()) {
+	  //     canContinue = false;
+	  //   }
+	  // }
+	  // jQuery.sap.require("sap.ca.ui.message.message");
+	  // if(canContinue){
+	  //   sap.ca.ui.message.showMessageBox({
+	  //     type : sap.ca.ui.message.Type.SUCCESS,
+	  //     message : "SUCCESS - Proceed"
+	  //   });
+	  // }else {
+	  //   sap.ca.ui.message.showMessageBox({
+	  //     type : sap.ca.ui.message.Type.WARNING,
+	  //     message : "WARNING - Empty Field(s)"
+	  //   });
+	  // }
 	}
 });
